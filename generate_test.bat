2dadebc5f8c5f47fd01a4f92f7b99953664a633b
Akky AKIMOTO <akky@users.noreply.github.com>
2016-03-09 17:39:29 +0900

    changed phpunit test generator command and paths.

--- generate_test.bat
+++ generate_test.bat
@@ -1,25 +1,24 @@
-call phpunit-skelgen --test -- Akky\Money\ExchangeRate vendor\Akky\Money\lib\Akky\Money\ExchangeRate.php Akky\Money\ExchangeRateTest tests/ExchangeRateTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\ExchangeRate includes\Akky\Money\ExchangeRate.php Akky\Money\ExchangeRateTest tests/ExchangeRateTest.php
 if not %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen --test --bootstrap tests\bootstrap.php -- Akky\Money\Usd vendor\Akky\Money\lib\Akky\Money\Usd.php Akky\Money\UsdTest tests/UsdTest.php
+call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Usd includes\Akky\Money\Usd.php Akky\Money\UsdTest tests/UsdTest.php
 if not %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen --test --bootstrap tests\bootstrap.php -- Akky\Money\Jpy vendor\Akky\Money\lib\Akky\Money\Jpy.php Akky\Money\JpyTest tests/JpyTest.php
+call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Jpy includes\Akky\Money\Jpy.php Akky\Money\JpyTest tests/JpyTest.php
 if not %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen --test -- Akky\Money\JpyFormatter vendor\Akky\Money\lib\Akky\Money\JpyFormatter.php Akky\Money\JpyFormatterTest tests/JpyFormatterTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\JpyFormatter includes\Akky\Money\JpyFormatter.php Akky\Money\JpyFormatterTest tests/JpyFormatterTest.php
 if not %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen --test -- Akky\Money\UsdFormatter vendor\Akky\Money\lib\Akky\Money\UsdFormatter.php Akky\Money\UsdFormatterTest tests/UsdFormatterTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\UsdFormatter includes\Akky\Money\UsdFormatter.php Akky\Money\UsdFormatterTest tests/UsdFormatterTest.php
 if not %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
-
