465230ad2a4ecd9fee06888f7dfffab452b51363
akky <akimoto+github@gmail.com>
2017-11-09 16:49:13 +0900

    some phpunit tests failed, as the test asks too much than the current code can parse.

--- generate_test.bat
+++ generate_test.bat
@@ -1,24 +1,24 @@
-call phpunit-skelgen  generate-test -- Akky\Money\ExchangeRate includes\Akky\Money\ExchangeRate.php Akky\Money\ExchangeRateTest tests/ExchangeRateTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\ExchangeRate includes\Akky\Money\ExchangeRate.php Akky\Money\ExchangeRateTest tests/phpunit/ExchangeRateTest.php
 if not %ERRORLEVEL% == 0 (
-	exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Usd includes\Akky\Money\Usd.php Akky\Money\UsdTest tests/UsdTest.php
+call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Usd includes\Akky\Money\Usd.php Akky\Money\UsdTest tests/phpunit/UsdTest.php
 if not %ERRORLEVEL% == 0 (
-	exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Jpy includes\Akky\Money\Jpy.php Akky\Money\JpyTest tests/JpyTest.php
+call phpunit-skelgen  generate-test --bootstrap tests\bootstrap.php -- Akky\Money\Jpy includes\Akky\Money\Jpy.php Akky\Money\JpyTest tests/phpunit/JpyTest.php
 if not %ERRORLEVEL% == 0 (
-	exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen  generate-test -- Akky\Money\JpyFormatter includes\Akky\Money\JpyFormatter.php Akky\Money\JpyFormatterTest tests/JpyFormatterTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\JpyFormatter includes\Akky\Money\JpyFormatter.php Akky\Money\JpyFormatterTest tests/phpunit/JpyFormatterTest.php
 if not %ERRORLEVEL% == 0 (
-	exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
 
-call phpunit-skelgen  generate-test -- Akky\Money\UsdFormatter includes\Akky\Money\UsdFormatter.php Akky\Money\UsdFormatterTest tests/UsdFormatterTest.php
+call phpunit-skelgen  generate-test -- Akky\Money\UsdFormatter includes\Akky\Money\UsdFormatter.php Akky\Money\UsdFormatterTest tests/phpunit/UsdFormatterTest.php
 if not %ERRORLEVEL% == 0 (
-	exit /b %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
